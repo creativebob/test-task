3983e3e7bec8d7eeaa10d7e389779d8f24a7456d
Creativebob <creativebob@mail.ru>
2023-03-09 21:33:50 +0800

    commit voyager

---
+++ app/Http/Controllers/Voyager/VoyagerPostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Voyager;
+
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
+
+use Illuminate\Http\Request;
+
+class VoyagerPostController extends VoyagerBaseController
+{
+
+    // public function index(Request $request)
+    // {
+    //     dd('Мы находимся здесь!');
+    // }
+
+}
